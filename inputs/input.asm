285d43db1dccdb0f98140438a6fe4b10aaeeacda
Minseok <mchu7797@gmail.com>
2023-11-07 11:26:28 +0900

    edited input assembly file for test harder

--- inputs/input.asm
+++ inputs/input.asm
@@ -1,28 +1,51 @@
-HELLO MACRO
-    HELLO
-    WORLDS
-HELLO ENDM
-HI MACRO
-    HI
-    WORLDS
-HI ENDM
-MAIN SEGMENT
-ASSUME CS:MAIN, DS:MAIN
-    MOV AX, CS
-    MOV DS, AX
-    MOV CX, 1
-    MOV AX, 0
-LOOP1:
-    CALL HELLO
-    ADD AX, CX
-    INC CX
-    CMP CX, 10
-    JBE LOOP1
-    MOV SUM, AX
-    MOV AH, 4CH
-    INT 21H
-    SUM DW ?
-    CALL HELLO
-    CALL HI
-MAIN ENDS
+M_LOOP          MACRO
+LOOP:           CMP AX, 8
+                JB COMPARE
+                JMP EXIT
+M_LOOP          ENDM
+
+M_COMPARE       MACRO
+COMPARE:        MOV SI, AX
+                MOV DL, DATA1[SI]
+                ADD DL, DATA2[SI]
+                CMP DL, 100
+                JB MOVE_TO_DMINS
+                JMP MOVE_TO_DMAXS
+M_COMPARE       ENDM
+
+M_EXIT          MACRO
+EXIT:           MOV AH, 2
+                INT 21H
+M_EXIT          ENDM
+
+MAIN            SEGMENT
+                ASSUME CS:MAIN, DS:MAIN
+                MOV AX, 0
+                MOV BX, 0
+                MOV CX, 0
+
+                CALL M_LOOP
+
+                CALL M_COMPARE
+
+MOVE_TO_DMAXS:  MOV SI, BX
+                MOV DMAXS[SI], DL
+                INC AX
+                INC BX
+                JMP LOOP
+
+MOVE_TO_DMINS:  MOV SI, CX
+                MOV DMINS[SI], DL
+                INC AX
+                INC CX
+                JMP LOOP
+
+                CALL M_EXIT
+
+DATA1           DB 35, 26, 20, 3, 100, 97, 88, 15
+DATA2           DB 15, 36, 64, 90, 10, 21, 32, 1
+DMAXS           DB 8 DUP(0)
+DMINS           DB 8 DUP(0)
+
+MAIN            ENDS
 END
